048a361631b66d2afb23d6de6c4ab4890bbd3dc5
kassidyr <kassidy.ralph@gmail.com>
2021-11-03 15:52:29 -0400

    add inquirer prompt

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-const fs = require ('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answer => console.log(answers));
+// const fs = require ('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//   if (err) throw err;
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-  if (err) throw err;
+//   console.log ('Portfolio complete! Check out index.html to see the output!')
+// });
 
-  console.log ('Portfolio complete! Check out index.html to see the output!')
-});
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
